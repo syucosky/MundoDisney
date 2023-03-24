2646c42035c14fc3530873358c0835b85e60a868
syucosky <syucosky@gmail.com>
2023-03-24 20:57:12 -0300

    Change @Controller to @RestController

--- api/src/main/java/com/MundoDisney/api/controller/MovieController.java
+++ api/src/main/java/com/MundoDisney/api/controller/MovieController.java
@@ -2,9 +2,9 @@ package com.MundoDisney.api.controller;
 
 import com.MundoDisney.api.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MovieController {
 
     @Autowired
