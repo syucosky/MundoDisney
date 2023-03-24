751300c5d580fc5ffbfc3872a55af263e07e0270
syucosky <syucosky@gmail.com>
2023-03-24 19:03:04 -0300

    layer of service without modules

---
+++ api/src/main/java/com/MundoDisney/api/service/GenderService.java
@@ -0,0 +1,14 @@
+package com.MundoDisney.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenderService {
+
+    @Autowired
+    GenderService genderService;
+
+
+
+}
